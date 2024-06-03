071e5f10af43d1a72968378b6401ab991f4f6b32
Mikhail Zavyalov <mikhailzavyalov0807@gmail.com>
2024-06-03 23:12:52 +0400

    work on network service

--- Air tickets/NetworkService/DTO/AllTicketsDTO.swift
+++ Air tickets/NetworkService/DTO/AllTicketsDTO.swift
@@ -7,7 +7,7 @@ struct AllTicketsDTO: Decodable {
 
 struct TicketDTO: Decodable {
     let id: Int
-    let badge: String
+    let badge: String?
     let price: PriceDTO
     let departure: DepartureDTO
     let arrival: ArrivalDTO
@@ -18,12 +18,57 @@ struct TicketDTO: Decodable {
     }
 
     struct DepartureDTO: Decodable {
-        let date: String
+        let date: Date
         let airport: String
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let dateString = try container.decode(String.self, forKey: .date)
+            date = try Date(dateString + "Z", strategy: .iso8601)
+            airport = try container.decode(String.self, forKey: .airport)
+        }
+
+        private enum CodingKeys: String, CodingKey {
+            case date
+            case airport
+        }
     }
 
     struct ArrivalDTO: Decodable {
-        let date: String
+        let date: Date
         let airport: String
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let dateString = try container.decode(String.self, forKey: .date)
+            date = try Date(dateString + "Z", strategy: .iso8601)
+            airport = try container.decode(String.self, forKey: .airport)
+        }
+
+        private enum CodingKeys: String, CodingKey {
+            case date
+            case airport
+        }
+    }
+}
+
+extension TicketDTO {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        badge = try container.decodeIfPresent(String.self, forKey: .badge)
+        price = try container.decode(PriceDTO.self, forKey: .price)
+        departure = try container.decode(DepartureDTO.self, forKey: .departure)
+        arrival = try container.decode(ArrivalDTO.self, forKey: .arrival)
+        has_transfer = try container.decode(Bool.self, forKey: .has_transfer)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case badge
+        case price
+        case departure
+        case arrival
+        case has_transfer
     }
 }
